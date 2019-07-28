071765576f1847711d520e96117dc5142f1c3f0d
fpdjsns <withham1@naver.com>
2019-07-29 01:27:23 +0900

    add KafkaProducer Callback

--- src/main/kotlin/com/bae/kafkaTest/producer/KafkaProducer.kt
+++ src/main/kotlin/com/bae/kafkaTest/producer/KafkaProducer.kt
@@ -1,7 +1,9 @@
 package com.bae.kafkaTest.producer
 
+import org.apache.kafka.clients.producer.Callback
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.clients.producer.RecordMetadata
 import org.springframework.stereotype.Component
 import java.util.*
 
@@ -16,9 +18,29 @@ class KafkaProducer(val props: Properties = Properties()){
     props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
   }
 
-  fun sendMessage(message: String){
+  fun sendMessage(message: String): Boolean {
     val producer = KafkaProducer<String, String>(props)
-    producer.send(ProducerRecord<String, String>(TOPIC_NAME, message))
-    producer.close()
+    try {
+      producer.send(ProducerRecord<String, String>(TOPIC_NAME, message), KafkaCallBack())
+      return true
+    } catch(e: Exception){
+      e.printStackTrace()
+    } finally {
+      producer.close()
+    }
+    return false
+  }
+
+  private class KafkaCallBack : Callback {
+    override fun onCompletion(metadata: RecordMetadata?, exception: java.lang.Exception?) {
+      metadata?.let {
+        print("""
+          Partition : ${metadata.partition()}
+          Offset : ${metadata.offset()}
+        """.trimIndent())
+      } ?: run {
+        exception!!.printStackTrace()
+      }
+    }
   }
 }
